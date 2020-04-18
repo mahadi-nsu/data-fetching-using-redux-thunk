3145e4bce7b2756e3db2ba208cd30dfb07f41a8d
mahadi-nsu <mahadi.hassan@northsouth.edu>
2020-04-18 11:07:18 +0600

    onclick data fetch added

--- src/componnets/PostList.js
+++ src/componnets/PostList.js
@@ -2,18 +2,27 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux';
 
 import { fetchPosts } from '../actions';
+import reducers from '../reducers';
+
 
 class PostList extends Component {
-    componentDidMount() {
-        console.log(this.props.fetchPosts());
-    }
+    // componentDidMount() {
+    //     console.log(this.props.fetchPosts());
+    // }
     render() {
+        console.log(this.props.posts.data);
         return (
             <div>
-                post list
+                <button onClick={() => { this.props.fetchPosts() }}>fetch datas</button>
             </div>
         )
     }
 }
 
-export default connect(null, { fetchPosts })(PostList);
+const mapStateToProps = (state) => {
+    return {
+        posts: state.posts,
+    }
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostList);
