b46d05a918525e907a1142da9119be4282807921
mahadi-nsu <mahadi.hassan@northsouth.edu>
2020-04-18 12:34:50 +0600

    post list displayed

--- src/componnets/PostList.js
+++ src/componnets/PostList.js
@@ -6,17 +6,29 @@ import reducers from '../reducers';
 
 
 class PostList extends Component {
-    // componentDidMount() {
-    //     console.log(this.props.fetchPosts());
-    // }
-    postfetch() {
+    componentDidMount() {
         this.props.fetchPosts();
     }
+    renderList() {
+        return this.props.posts.map(post => {
+            return (
+                <div className="item" key="post.id">
+                    <i className="large middle aligned icon user" />
+                    <div className="content">
+                        <div className="description">
+                            <h2>{post.title}</h2>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                </div>
+            )
+        })
+    }
     render() {
-        console.log(this.props.posts.data);
+        console.log(this.props.posts);
         return (
-            <div>
-                <button onClick={() => { this.postfetch() }}>fetch datas</button>
+            <div className = "ui relaxed divided list">
+                {this.renderList()}
             </div>
         )
     }
