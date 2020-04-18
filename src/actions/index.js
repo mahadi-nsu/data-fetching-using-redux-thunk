c3b9d7869eb1576090a94e125dad05669bbb7e95
mahadi-nsu <mahadi.hassan@northsouth.edu>
2020-04-19 01:04:49 +0600

    memoization added to reduce api calls

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
-import jsonPlaceholder from "../apis/jsonPlaceholder"
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+import _ from 'lodash';
 
 export const fetchPosts = () => {
     return async (dispatch) => {
@@ -10,16 +11,29 @@ export const fetchPosts = () => {
     }
 }
 
-export const fetchUsers = (id) => {
-    return async (dispatch) => {
-        const response = await jsonPlaceholder.get(`/users/${id}`);
-        dispatch({
-            type: 'FETCH_USER',
-            payload: response.data
-        })
-    }
+// export const fetchUsers = (id) => {
+//     return async (dispatch) => {
+//         const response = await jsonPlaceholder.get(`/users/${id}`);
+//         dispatch({
+//             type: 'FETCH_USER',
+//             payload: response.data
+//         })
+//     }
 
+// }
+
+//sytax change
+export const fetchUsers = id => dispatch => {
+    _fetchUser(id, dispatch);
 }
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+    dispatch({
+        type: 'FETCH_USER',
+        payload: response.data
+    })
+});
 
 // const response =  jsonPlaceholder.get('/posts');
 //     return {
